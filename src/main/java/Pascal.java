690fbb48daa2a0347b07e86cf90c296cb2316e70
nmakdessi <21nmakdessi@athenian.org>
2021-01-19 20:29:18 +0000

    completed

--- src/main/java/Pascal.java
+++ src/main/java/Pascal.java
@@ -7,7 +7,19 @@
 public class Pascal
 {
     public static int[][] pascalTriangle(int n) {
-        // YOUR CODE HERE
-        return null;
+        int [][] output = new int [n][n];
+        output [0][0] = 1;
+        for (int i = 1; i < n; i++){
+            output[i][0] = 1;
+            output[i][i] = 1;
+        }
+        for (int i = 2; i < n; i++){
+            for (int j = 1; j < i; j++){
+                output[i][j] = output[i-1][j-1] + output[i-1][j];
+            }
+        }
+        return output;
     }
+
+     
 }
